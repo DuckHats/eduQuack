3623258a817477fce7e3e7061d1985dabd668873
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-09 17:55:43 +0200

    login vell

--- Duckhat/login.php
+++ Duckhat/login.php
@@ -1,11 +1,10 @@
 <?php
-session_start();
 include('config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtener y validar datos del formulario
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    // Obtener datos del formulario
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     // Conectar a la base de datos
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -22,22 +21,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['password'])) {
-            // Inicio de sesión exitoso, almacena la información del usuario en la sesión
-            $_SESSION['usuario_id'] = $row['id'];
-            $_SESSION['email'] = $row['email'];
-            $_SESSION['curso_id'] = $row['curso_id']; // Suponiendo que hay un campo 'curso_id' en tu tabla de usuarios
-
-            // Redirigir al usuario a la página de noticias después del inicio de sesión exitoso
-            header("Location: news.html");
+            // Inicio de sesión exitoso, redirigir al usuario a la página de bienvenida
+            header("Location: bienvenido.php");
             exit();
         } else {
             // Contraseña incorrecta, redirigir de vuelta a la página de inicio de sesión con un mensaje de error
-            header("Location: login.html?error=incorrecto");
+            header("Location: login.html?error=contrasena");
             exit();
         }
     } else {
         // Usuario no encontrado, redirigir de vuelta a la página de inicio de sesión con un mensaje de error
-        header("Location: login.html?error=incorrecto");
+        header("Location: login.html?error=usuario");
         exit();
     }
 
