b624b171d2035e983e50bffa5323aca87a26aea2
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-10 02:39:14 +0200

    mitj tutorial 2

--- Duckhat/register.php
+++ Duckhat/register.php
@@ -15,70 +15,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     // $username = $_POST['username'];
     // $cursoId = $_POST['curso_id'];
     // $fechaNacimiento = $_POST['fecha_nacimiento'];
-    
-                    // A partir de aqui
-                    
+
     // Prepara la consulta SQL para verificar si el nombre de usuario ya está en uso
-    $sql = "SELECT id FROM users WHERE username = ?";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        // Vincula las variables a la declaración preparada como parámetros
-        mysqli_stmt_bind_param($stmt, "s", $param_username);
-        
-        // Establece los parámetros
-        $param_username = $username;
-        
-        // Intenta ejecutar la declaración preparada
-        if(mysqli_stmt_execute($stmt)){
-            // Almacena el resultado
-            mysqli_stmt_store_result($stmt);
-            
-            if(mysqli_stmt_num_rows($stmt) == 1){
-                // Nombre de usuario ya en uso, muestra un mensaje de error
-                header("Location: register.html?error=usuarioexistente");
-                exit();
-            } else{
-                // Nombre de usuario no está en uso, procede a registrar el usuario
-                // Prepara la declaración SQL para insertar datos en la tabla users
-                $sql = "INSERT INTO users (username, curso_id, email, fecha_nacimiento, password) VALUES (?, ?, ?, ?, ?)";
-                 
-                if($stmt = mysqli_prepare($link, $sql)){
-                    // Vincula las variables a la declaración preparada como parámetros
-                    mysqli_stmt_bind_param($stmt, "sssss", $param_username, $param_cursoId, $param_email, $param_fechaNacimiento, $param_password);
-                    
-                    // Establece los parámetros
-                    $param_username = $username;
-                    $param_cursoId = $cursoId;
-                    $param_email = $email;
-                    $param_fechaNacimiento = $fechaNacimiento;
-                    $param_password = password_hash($password, PASSWORD_DEFAULT); // Crea un hash de contraseña
-                    
-                    // Intenta ejecutar la declaración preparada
-                    if(mysqli_stmt_execute($stmt)){
-                        // Registro exitoso, redirige al usuario a la página de inicio de sesión
-                        header("Location: login.html");
-                        exit();
-                    } else{
-                        // Error al registrar el usuario, muestra un mensaje de error
-                        header("Location: register.html?error=registrofallido");
-                        exit();
-                    }
-
-                    // Cierra la declaración
-                    mysqli_stmt_close($stmt);
-                }
-            }
-        } else{
-            // Error en la declaración preparada, muestra un mensaje de error
-            header("Location: register.html?error=errordeconsulta");
-            exit();
+    if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+        $error = '';
+    $query->bind_param('s', $email);
+    $query->execute();
+    //guardar
+    $query->store_result();
+        if ($query->num_rows > 0) {
+                // A partir de aqui
         }
-
-        // Cierra la declaración
-        // mysqli_stmt_close($stmt);
     }
     
-    // Cierra la conexión
-    mysqli_close($link);
-}
 ?>
