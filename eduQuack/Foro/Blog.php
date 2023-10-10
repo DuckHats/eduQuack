14437d6bdc05c22dd577ceeb7627cf32d48f169c
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-11 01:45:12 +0200

    blog fora de l'index i amb img

--- eduQuack/Foro/Blog.php
+++ eduQuack/Foro/Blog.php
@@ -7,12 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contenido = $_POST["contenido"];
     $autor = $_SESSION["username"];
 
-    $imagen_path = "uploads/images/" . $_FILES["imagen"]["name"];
-    move_uploaded_file($_FILES["imagen"]["tmp_name"], $imagen_path);
+    // Verificar si se ha subido un archivo de imagen (puedes agregar más validaciones aquí)
+    if ($_FILES["imagen"]["error"] == UPLOAD_ERR_OK) {
+        $imagen_path = "uploads/images/" . $_FILES["imagen"]["name"];
+        move_uploaded_file($_FILES["imagen"]["tmp_name"], $imagen_path);
+    } else {
+        $imagen_path = ""; // Si no se subió una imagen, asigna un valor predeterminado o una cadena vacía
+    }
 
+    // Insertar el post en la base de datos
     $sql = "INSERT INTO posts (title, content, image_path, author) VALUES ('$titulo', '$contenido', '$imagen_path', '$autor')";
+
     if ($conn->query($sql) === TRUE) {
-        header("Location: Blog.php");
+        header("Location: blog.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -38,7 +45,7 @@ $result = $conn->query($sql);
 
         <!-- Formulario para crear un nuevo post -->
         <h2>Nuevo Post</h2>
-        <form action="Blog.php" method="post" enctype="multipart/form-data">
+        <form action="blog.php" method="post" enctype="multipart/form-data">
             <label for="titulo">Título:</label>
             <input type="text" id="titulo" name="titulo" required><br>
             <label for="contenido">Contenido:</label><br>
--- eduQuack/Foro/crear_post.php
+++ eduQuack/Foro/crear_post.php
@@ -1,28 +1,28 @@
 <?php
 session_start();
-require_once('blog_database.php');
+require_once('database.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $titulo = $_POST["titulo"];
     $contenido = $_POST["contenido"];
     $autor = $_SESSION["username"];
 
-    // Verificar si se ha subido un archivo de imagen
+    // Verificar si se ha subido un archivo de imagen (puedes agregar más validaciones aquí)
     if ($_FILES["imagen"]["error"] == UPLOAD_ERR_OK) {
         $imagen_path = "uploads/images/" . $_FILES["imagen"]["name"];
         move_uploaded_file($_FILES["imagen"]["tmp_name"], $imagen_path);
     } else {
-        // Si no se subió una imagen, puedes asignar un valor predeterminado o dejarlo como una cadena vacía
-        $imagen_path = ""; // Asigna un valor predeterminado o una cadena vacía según tu preferencia
+        $imagen_path = ""; // Si no se subió una imagen, asigna un valor predeterminado o una cadena vacía
     }
 
     // Insertar el post en la base de datos
     $sql = "INSERT INTO posts (title, content, image_path, author) VALUES ('$titulo', '$contenido', '$imagen_path', '$autor')";
-    
+
     if ($conn->query($sql) === TRUE) {
-        header("Location: Blog.php");
+        header("Location: blog.php");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 ?>
+
