7c1f6698aedc6f76a49cb3d40da1bafb98cedbf5
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-11 13:19:44 +0200

    blog vell

--- eduQuack/Foro/Blog.php
+++ eduQuack/Foro/Blog.php
@@ -1,72 +1,82 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.html");
+    exit;
+}
+
+require_once('blog_database.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titulo = $_POST["titulo"];
+    $contenido = $_POST["contenido"];
+    $autor = $_SESSION["username"];
+
+    if ($_FILES["imagen"]["error"] == UPLOAD_ERR_OK) {
+        $imagen_path = "uploads/images/" . $_FILES["imagen"]["name"];
+        move_uploaded_file($_FILES["imagen"]["tmp_name"], $imagen_path);
+    } else {
+        $imagen_path = "";
+    }
+
+    $sql = "INSERT INTO posts (title, content, image_path, author) VALUES ('$titulo', '$contenido', '$imagen_path', '$autor')";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: Blog.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$sql = "SELECT * FROM posts ORDER BY created_at DESC";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Mi Blog | eduQuack</title>
-    <link rel="icon" href="../images/ginebro-logo (1).png">
-    <link rel="stylesheet" href="../css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Blog</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
+    <div class="container">
+        <h1>Mi Blog</h1>
 
-    <menu>
-        <img src="images/ginebro-logo (1).png">
-        <ul>
-            <li><a href="../index.php"><h3 class="negrita">Menú</h3></a></li>
-            <li><a href="../teams.php"><h3>Grupos</h3></a></li>
-            <li><a href="../news.php"><h3>Noticias</h3></a></li>
-            <li><a href="./Foro/Blog.php"><h3>Foro</h3></a></li>
-            <li><a href="../perfil.php"><img id="conficon" src="../images/user.png"></a></li>
-        </ul>
-    </menu>
+        <!-- Formulario para crear un nuevo post -->
+        <h2>Nuevo Post</h2>
+        <form action="Blog.php" method="post" enctype="multipart/form-data">
+            <label for="titulo">Título:</label>
+            <input type="text" id="titulo" name="titulo" required><br>
+            <label for="contenido">Contenido:</label><br>
+            <textarea id="contenido" name="contenido" rows="4" cols="50" required></textarea><br>
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" name="imagen"><br>
+            <input type="submit" value="Publicar">
+        </form>
 
-    <h1 class="my-5">Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenido a nuestro blog.</h1>
-
-    <main>
-        <!-- Contenido personalizado para la página de bienvenida -->
-        <div class="welcome-message">
-            <h1>¡Bienvenido a nuestro blog, <?php echo htmlspecialchars($_SESSION["username"]); ?>!</h1>
-            <p>¡Gracias por iniciar sesión en eduQuack! Explora y comparte tus pensamientos con la comunidad.</p>
-            <!-- Puedes agregar más contenido personalizado aquí según tus necesidades -->
-
-            <!-- Formulario para crear un nuevo post -->
-            <h2>Nuevo Post</h2>
-            <form action="Blog.php" method="post" enctype="multipart/form-data">
-                <label for="titulo">Título:</label>
-                <input type="text" id="titulo" name="titulo" required><br>
-                <label for="contenido">Contenido:</label><br>
-                <textarea id="contenido" name="contenido" rows="4" cols="50" required></textarea><br>
-                <label for="imagen">Imagen:</label>
-                <input type="file" id="imagen" name="imagen"><br>
-                <input type="submit" value="Publicar">
-            </form>
-
-            <!-- Mostrar los posts del blog -->
-            <?php foreach ($noticias as $noticia): ?>
-                <div class="post">
-                    <h2><?php echo htmlspecialchars($noticia['titulo']); ?></h2>
-                    <p>Autor: <?php echo htmlspecialchars($noticia['autor']); ?></p>
-                    <p><?php echo htmlspecialchars($noticia['contenido']); ?></p>
-                    <?php if (!empty($noticia['imagen_path'])): ?>
-                        <img src="<?php echo htmlspecialchars($noticia['imagen_path']); ?>" alt="Imagen del post">
-                    <?php endif; ?>
-
-                    <!-- Botón para eliminar el post (envía el ID del post a través de la URL) -->
-                    <a href="borrar_post.php?id=<?php echo $noticia['id']; ?>">Eliminar</a>
-
-                    <!-- Enlace para ver más detalles del post -->
-                    <a href="thread.php?id=<?php echo $noticia['id']; ?>">Ver Más</a>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </main>
-
-    <footer>
-        <a href="../eduQuack/Legal/License.pdf">Todos los derechos reservados a eduQuack</a>
-        <p>Contáctenos en el correo <a href="mailto:example@ginebro.cat">example@ginebro.cat</a></p>
-    </footer>
-
+        <!-- Mostrar los posts del blog -->
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <div class="post">
+                <h2><?= $row["title"] ?></h2>
+                <p>Autor: <?= $row["author"] ?></p>
+                <p><?= $row["content"] ?></p>
+                <?php if ($row["image_path"]) : ?>
+                    <img src="<?= $row["image_path"] ?>" alt="Imagen del post">
+                <?php endif; ?>
+                
+                <!-- Botón para eliminar el post (envía el ID del post a través de la URL) -->
+                <a href="borrar_post.php?id=<?= $row["id"] ?>">Eliminar</a>
+                
+                <!-- Enlace para ver más detalles del post -->
+                <a href="thread.php?id=<?= $row["id"] ?>">Ver Más</a>
+            </div>
+        <?php endwhile; ?>
+    </div>
 </body>
 
 </html>
