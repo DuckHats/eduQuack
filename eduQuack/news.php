14fccf3c00522dccf8d594377f5e16c8ab6e3f8e
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-11 18:05:27 +0200

    news.php paginació

--- eduQuack/news.php
+++ eduQuack/news.php
@@ -1,28 +1,35 @@
 <?php
 session_start();
 
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+// Verificar inicio de sesión
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.html");
     exit;
 }
 
+// Incluir archivo de configuración de la base de datos
 require_once('news_database.php');
-// $servername = "localhost";
-// $username = "root";
-// $password = "1234";
-// $database = "noticias_db";
 
-// Crea la conexión
-$conn = new mysqli($servername, $username, $password, $database);
+// Configuración para paginación
+$noticiasPorPagina = 5;
+$paginaActual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$inicio = ($paginaActual - 1) * $noticiasPorPagina;
 
-// Verifica la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+// Obtener el filtro del formulario (si está presente)
+$filtroCurso = isset($_GET['curso']) ? $_GET['curso'] : 'todos';
+$filtroConsulta = ($filtroCurso !== 'todos') ? "WHERE curso = '$filtroCurso'" : '';
 
-// Consulta SQL para obtener todas las noticias
-$sql = "SELECT titulo, contenido, fecha FROM noticias";
+// Consulta SQL con paginación y filtro por curso
+$sql = "SELECT titulo, contenido, fecha FROM noticias $filtroConsulta ORDER BY fecha DESC LIMIT $inicio, $noticiasPorPagina";
 $result = $conn->query($sql);
+
+// Consulta para contar el total de noticias con el filtro
+$sqlTotalNoticias = "SELECT COUNT(*) as total FROM noticias $filtroConsulta";
+$resultTotalNoticias = $conn->query($sqlTotalNoticias);
+$totalNoticias = $resultTotalNoticias->fetch_assoc()['total'];
+
+// Calcular el total de páginas
+$totalPaginas = ceil($totalNoticias / $noticiasPorPagina);
 
 // Array para almacenar las noticias
 $noticias = [];
@@ -40,47 +47,57 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <!-- Configuración de la codificación de caracteres y el tamaño de la ventana de visualización -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Título de la página y enlace al icono en la pestaña del navegador -->
     <title>News | eduQuack</title>
     <link rel="icon" href="images/ginebro-logo (1).png">
-    <!-- Enlace al archivo de estilos CSS -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     <!-- Menú de navegación -->
     <menu>
-        <img src="images/ginebro-logo (1).png">
-        <ul>
-            <!-- Enlaces a otras páginas del sitio web -->
-            <li><a href="index.php"><h3>Menú</h3></a></li>
-            <li><a href="teams.php"><h3>Grups</h3></a></li>
-            <li><a href="news.php"><h3 class="negrita">Notícies</h3></a></li>
-            <li><a href="./Foro/Blog.php"><h3>Forum</h3></a></li>
-            <li><a href="perfil.php"><img id="conficon" src="images/user.png"></a></li>
-        </ul>
+        <!-- Tu menú aquí -->
     </menu>
     <main>
         <!-- Contenedor para mostrar las noticias dinámicamente -->
-        <div id="noticias-container"></div>
-        <?php foreach ($noticias as $noticia): ?>
-        <div class="noticia">
-            <h2><?php echo htmlspecialchars($noticia['titulo']); ?></h2>
-            <p><?php echo htmlspecialchars($noticia['contenido']); ?></p>
-            <p>Fecha: <?php echo htmlspecialchars($noticia['fecha']); ?></p>
+        <div id="noticias-container">
+            <?php foreach ($noticias as $noticia): ?>
+                <div class="noticia">
+                    <h2><?php echo htmlspecialchars($noticia['titulo']); ?></h2>
+                    <p><?php echo htmlspecialchars($noticia['contenido']); ?></p>
+                    <p>Fecha: <?php echo htmlspecialchars($noticia['fecha']); ?></p>
+                </div>
+            <?php endforeach; ?>
         </div>
-    <?php endforeach; ?>
 
-        <!-- Código JavaScript para cargar dinámicamente las noticias usando AJAX -->
+        <!-- Paginación -->
+        <div class="pagination">
+            <?php for ($i = 1; $i <= $totalPaginas; $i++): ?>
+                <a href="?pagina=<?php echo $i . '&curso=' . $filtroCurso; ?>" <?php if ($i == $paginaActual) echo 'class="active"'; ?>><?php echo $i; ?></a>
+            <?php endfor; ?>
+        </div>
+
+        <!-- Filtro por curso -->
+        <form action="" method="get" id="filtro-form">
+            <label for="curso">Filtrar por curso:</label>
+            <select id="curso" name="curso">
+                <option value="todos" <?php if ($filtroCurso === 'todos') echo 'selected'; ?>>Todos</option>
+                <option value="curso1" <?php if ($filtroCurso === 'curso1') echo 'selected'; ?>>Curso 1</option>
+                <option value="curso2" <?php if ($filtroCurso === 'curso2') echo 'selected'; ?>>Curso 2</option>
+                <option value="curso3" <?php if ($filtroCurso === 'curso3') echo 'selected'; ?>>Curso 3</option>
+                <!-- Agrega más opciones de cursos según sea necesario -->
+            </select>
+            <input type="submit" value="Filtrar">
+        </form>
     </main>
+
     <!-- Pie de página -->
     <footer>
-        <!-- Enlace al archivo de licencia y dirección de correo electrónico de contacto -->
-        <a href="../eduQuack/Legal/License.pdf">Tots els drets reservats a eduQuack</a>
-        <p>Contactens al correu <a>example@ginebro.cat</a></p>
+        <!-- Tu pie de página aquí -->
     </footer>
 </body>
+
 </html>
