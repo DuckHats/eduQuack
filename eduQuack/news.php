91d62052db945d475fa92e3d7d6039cfe17aec19
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-11 13:10:57 +0200

    news.php

--- eduQuack/news.php
+++ eduQuack/news.php
@@ -1,40 +1,40 @@
 <?php
 session_start();
-include('news_database.php');
 
-// Comprobar si el usuario ha iniciado sesión
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.html");
     exit;
 }
 
-// Establecer la conexión a la base de datos
-$conn = new mysqli($servername, $username, $password, $dbname);
+require_once('news_database.php');
+// $servername = "localhost";
+// $username = "root";
+// $password = "1234";
+// $database = "noticias_db";
 
-// Comprobar la conexión
+// Crea la conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Verifica la conexión
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener las noticias
+// Consulta SQL para obtener todas las noticias
 $sql = "SELECT titulo, contenido, fecha FROM noticias";
 $result = $conn->query($sql);
 
-// Crear un array para almacenar las noticias
+// Array para almacenar las noticias
 $noticias = [];
 
+// Obtener todas las noticias y guardarlas en el array $noticias
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        // Agrega cada noticia como un array asociativo al array de noticias
-        $noticias[] = array(
-            'titulo' => htmlspecialchars($row["titulo"]),
-            'contenido' => htmlspecialchars($row["contenido"]),
-            'fecha' => htmlspecialchars($row["fecha"])
-        );
+        $noticias[] = $row;
     }
 }
 
-// Cerrar la conexión a la base de datos
+// Cerrar la conexión
 $conn->close();
 ?>
 
@@ -49,8 +49,6 @@ $conn->close();
     <link rel="icon" href="images/ginebro-logo (1).png">
     <!-- Enlace al archivo de estilos CSS -->
     <link rel="stylesheet" href="./css/style.css">
-    <!-- Inclusión de jQuery para utilizar AJAX -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <!-- Menú de navegación -->
@@ -68,29 +66,15 @@ $conn->close();
     <main>
         <!-- Contenedor para mostrar las noticias dinámicamente -->
         <div id="noticias-container"></div>
+        <?php foreach ($noticias as $noticia): ?>
+        <div class="noticia">
+            <h2><?php echo htmlspecialchars($noticia['titulo']); ?></h2>
+            <p><?php echo htmlspecialchars($noticia['contenido']); ?></p>
+            <p>Fecha: <?php echo htmlspecialchars($noticia['fecha']); ?></p>
+        </div>
+    <?php endforeach; ?>
 
         <!-- Código JavaScript para cargar dinámicamente las noticias usando AJAX -->
-        <script>
-            $(document).ready(function() {
-                // Función para cargar noticias usando AJAX
-                function cargarNoticias() {
-                    // Llena dinámicamente las noticias en el contenedor
-                    var noticiasContainer = $('#noticias-container');
-                    noticiasContainer.empty(); // Limpia el contenedor
-                    <?php foreach ($noticias as $noticia): ?>
-                        // Crea un elemento de noticia y agrega al contenedor
-                        var noticiaElement = $('<div class="noticia">');
-                        noticiaElement.append('<h2><?php echo $noticia["titulo"]; ?></h2>');
-                        noticiaElement.append('<p><?php echo $noticia["contenido"]; ?></p>');
-                        noticiaElement.append('<p> <?php echo $noticia["fecha"]; ?>"</p>');
-                        noticiasContainer.append(noticiaElement);
-                    <?php endforeach; ?>
-                }
-
-                // Llama a la función para cargar noticias cuando la página se carga
-                cargarNoticias();
-            });
-        </script>
     </main>
     <!-- Pie de página -->
     <footer>
