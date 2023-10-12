f28dd3c6e77758198708eecb0ffb08fc83fb0b48
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-12 22:02:34 +0200

    logout

--- eduQuack/perfil.php
+++ eduQuack/perfil.php
@@ -47,7 +47,7 @@
                     <h3>Notícies</h3>
                 </a></li>
             <li><a href="./Foro/Blog.php">
-                <h3>Forum</h3>
+                    <h3>Forum</h3>
                 </a></li>
             <li><a href="perfil.php"><img id="conficon" src="images/userselec.png"></a></li>
         </ul>
@@ -65,41 +65,51 @@
         <div id="infoconf">
             <h2>Perfil</h2>
             <form action="update.php" method="post">
-            <table>
-        <tr>
-            <td><h3>Username</h3>
-                <input type="text" name="username" value="<?= htmlspecialchars($username); ?>"></td>
-            <td><h3>Nom complet</h3>
-                <input type="text" name="full_name" value="<?= htmlspecialchars($full_name); ?>"></td>
-        </tr>
-        <tr>
-            <td><p>ID de usuario: "<?= $id ?>"</p></td>
-            <td><p>Bienvenido session, <?php echo htmlspecialchars($_SESSION["id"]); ?>!</p></td>
-        </tr>
-        <tr>
-            <td><!-- <li>
+                <table>
+                    <tr>
+                        <td>
+                            <h3>Username</h3>
+                            <input type="text" name="username" value="<?= htmlspecialchars($username); ?>">
+                        </td>
+                        <td>
+                            <h3>Nom complet</h3>
+                            <input type="text" name="full_name" value="<?= htmlspecialchars($full_name); ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>ID de usuario: "<?= $id ?>"</p>
+                        </td>
+                        <td>
+                            <p>Bienvenido session, <?php echo htmlspecialchars($_SESSION["id"]); ?>!</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><!-- <li>
                     <h3>Número telefònic</h3>
                     <input type="tel" value="">
                 </li> --></td>
-            <td><img src="images/avatar.png" alt="avatar"></td>
-        </tr>
-        <tr>
-            <td><h3 id="email">E-mail</h3>
-                <input type="text" name="full_name" value="<?= htmlspecialchars($email); ?>"></td>
-        </tr>
-        <tr>
-            <td><!-- 
+                        <td><img src="images/avatar.png" alt="avatar"></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h3 id="email">E-mail</h3>
+                            <input type="text" name="full_name" value="<?= htmlspecialchars($email); ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><!-- 
             <h2 id="contrasenya">Contrasenya</h2>
             <input type="password" placeholder="Nova contrasenya">
             <input type="password" placeholder="Confirma la nova contrasenya"> --></td>
-            <td><img src="images/qr.png" alt="Qr"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Actualitzar"></td>
-    </table>
+                        <td><img src="images/qr.png" alt="Qr"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Actualitzar"></td>
+                </table>
             </form>
             <form action="logout.php" method="post">
-                <input id="true" type="submit" onclick="logout.php" value="logout">
+                <input type="submit" value="logout">
             </form>
         </div>
 
