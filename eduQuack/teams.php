a9f065a62fceba3e2e68fc8d4f746045dc26f9f4
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-12 11:34:46 +0200

    teams.php i teams.html

--- eduQuack/teams.php
+++ eduQuack/teams.php
@@ -1,39 +1,13 @@
 <?php
-session_start();
-
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    // Si el usuario no ha iniciado sesión, redirige a la página de login
     header("location: login.html");
     exit;
+} else {
+    header("location: teams.html");
+    exit;
+    // Si el usuario ha iniciado sesión, realiza alguna acción específica aquí
+    // Por ejemplo, mostrar un mensaje de bienvenida o cargar datos del usuario
+    // Puedes colocar tu código adicional aquí
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grups | eduQuack</title>
-    <link rel="icon" href="images/ginebro-logo (1).png">
-    <link rel="stylesheet" href="./css/style.css"> 
-</head>
-<body>
-    <menu>
-        <img src="images/ginebro-logo (1).png">
-        <ul>
-            <li><a href="index.php"><h3>Menú</h3></a></li>
-            <li><a href="teams.php"><h3 class="negrita">Grups</h3></a></li>
-            <li><a href="news.php"><h3>Notícies</h3></a></li>
-            <li><a href="./Foro/Blog.php"><h3>Forum</h3></a></li>
-            <li><a href="perfil.php"><img id="conficon" src="images/user.png"></a></li>
-        </ul>
-
-        
-    </menu>
-    
-    <footer>
-        <a href="../eduQuack/Legal/License.pdf">Tots els drets reservats a eduQuack</a>
-   <p>Contactens al correu <a>example@ginebro.cat</a></p>
-   </footer>
-</body>
-
-</html>
