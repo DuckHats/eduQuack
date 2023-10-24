699ba6c077aa64027db55e2f716e2596b5f19566
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-24 12:58:55 +0200

    admin news

--- eduQuack/admin_news/noticia.php
+++ eduQuack/admin_news/noticia.php
@@ -5,26 +5,32 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     exit;
 }
 
-require_once('news_database.php'); // Inclou el fitxer de configuració de la base de dades
+require_once('news_database.php');
 
-// Obté dades del formulari
+// Obtén datos del formulario
 $titulo = $_POST['titulo'];
 $contenido = $_POST['contenido'];
 $fecha = $_POST['fecha'];
 $curso = $_POST['curso'];
 
-// Processa la imatge
-// $imagen = $_FILES['imagen']['name'];
-// $imagen_temp = $_FILES['imagen']['tmp_name'];
-// move_uploaded_file($imagen_temp, "carpeta_imagenes/$imagen");
+// Consulta preparada para insertar datos en la tabla noticias
+$sql = "INSERT INTO noticias (titulo, contenido, fecha, curso) VALUES (?, ?, ?, ?)";
+if ($stmt = $conn->prepare($sql)) {
+    $stmt->bind_param("ssss", $titulo, $contenido, $fecha, $curso);
 
-// Inserta la noticia a la base de dades
-$sql = "INSERT INTO noticias (titulo, contenido, fecha, curso) VALUES ('$titulo', '$contenido', '$fecha', '$curso')";
-if ($mysqli->query($sql) === TRUE) {
-    echo "Noticia añadida correctamente. <a href='formulario_noticias.php'>Volver</a>";
+    // Ejecuta la consulta
+    if ($stmt->execute()) {
+        echo "Noticia añadida correctamente. <a href='formulario_noticias.php'>Volver</a>";
+    } else {
+        echo "Error al añadir la noticia: " . $stmt->error;
+    }
+
+    // Cierra la declaración
+    $stmt->close();
 } else {
-    echo "Error al añadir la noticia: " . $mysqli->error;
+    echo "Error en la preparación de la consulta: " . $conn->error;
 }
 
-$mysqli->close();
+// Cierra la conexión a la base de datos
+$conn->close();
 ?>
