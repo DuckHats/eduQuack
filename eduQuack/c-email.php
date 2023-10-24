bbad7171d7e4f5ecb44d2cf43eba6acfa7b19d09
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-24 12:16:37 +0200

    rpassword

--- eduQuack/c-email.php
+++ eduQuack/c-email.php
@@ -1,26 +1,30 @@
 <?php
 require_once "config.php";
 
+if($mysqli === false){
+    die("ERROR: No se pudo conectar a la base de datos. " . $mysqli->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
+    $email = $mysqli->real_escape_string($_POST['email']);
 
-    // Comprovar si l'email existeix a la base de dades
-    $sql = "SELECT id FROM users WHERE email = ?";
-    if ($stmt = $mysqli->prepare($sql)) {
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $stmt->store_result();
+    // Comprovem si l'email existeix a la base de dades
+    $consulta = $mysqli->query("SELECT * FROM usuaris WHERE correu = '$email'");
 
-        if ($stmt->num_rows > 0) {
-            // L'email existeix a la base de dades
-            // Redirigir a una altra pàgina per reiniciar la contrasenya
-            header("location: reset-password-form.php?email=$email");
-        } else {
-            // L'email no existeix a la base de dades
-            echo "Aquest email no està registrat.";
-        }
-
-        $stmt->close();
+    if ($consulta->num_rows > 0) {
+        // L'email existeix, permetem que l'usuari canviï la contrasenya
+        header("Location: canviar-contrasenya.php");
+        // Mostra un formulari perquè l'usuari introdueixi la nova contrasenya
+        // Amb el mètode POST, actualitza la contrasenya a la base de dades
+        // Mostra un missatge de confirmació a l'usuari
+    } else {
+        // L'email no existeix a la base de dades, redirigeix a una pàgina d'error
+        header("Location: error.php"); // redirigeix a la pàgina d'error
+        exit();
     }
+
+    // Tanquem la connexió a la base de dades
+    $mysqli->close();
 }
 ?>
+
---
+++ eduQuack/canviar-contrasenya.php
@@ -0,0 +1,19 @@
+<!-- canviar-contrasenya.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Metatags, títol, vincles a estils, etc. -->
+</head>
+<body>
+    <main class="loginmain">
+        <div class="info">
+            <h1>Canviar Contrasenya</h1>
+            <br>
+            <form action="processar-canvi-contrasenya.php" method="post">
+                <input type="password" placeholder="Nova Contrasenya" id="nova_contrasenya" name="nova_contrasenya">
+                <input type="submit" value="Canviar Contrasenya">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
