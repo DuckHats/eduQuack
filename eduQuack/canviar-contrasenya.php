a6dd54d36a29e7d41db243ed922b33d9c2e28fb5
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-24 12:23:49 +0200

    rpassword

--- eduQuack/canviar-contrasenya.php
+++ eduQuack/canviar-contrasenya.php
@@ -1,19 +1,31 @@
-<!-- canviar-contrasenya.php -->
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <!-- Metatags, títol, vincles a estils, etc. -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar Contraseña | eduQuack</title>
+    <link rel="icon" href="images/ginebro-logo (1).png">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <main class="loginmain">
+        <div class="ginebro">
+            <img id="logologin" src="../eduQuack/images/ginebro-logo-blanc-742059845.png">
+        </div>
+
         <div class="info">
-            <h1>Canviar Contrasenya</h1>
+            <h1>Recuperar Contraseña</h1>
             <br>
             <form action="processar-canvi-contrasenya.php" method="post">
                 <input type="password" placeholder="Nova Contrasenya" id="nova_contrasenya" name="nova_contrasenya">
                 <input type="submit" value="Canviar Contrasenya">
             </form>
         </div>
+
     </main>
 </body>
 </html>
+
+
+
+            
