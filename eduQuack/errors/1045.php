74c04bd4632dc55834089f3fd670fc79dd120abf
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-29 23:19:38 +0100

    errors

--- eduQuack/errors/1045.php
+++ eduQuack/errors/1045.php
@@ -4,13 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <title>1045 Error | EduQuack</title>
+    <link rel="icon" href="../images/errors/1045.jpg">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
     <h1>1045 Error: Accés denegat</h1>
     <p>No tens els permisos necessaris per realitzar aquesta acció.</p>
     <img src="../images/errors/1045.jpg" alt="1045 Error">
-    <a href="index.php"><button>Torna a la pàgina inicial</button></a>
+    <a href="../index.php"><button>Torna a la pàgina inicial</button></a>
 </body>
 
 </html>
