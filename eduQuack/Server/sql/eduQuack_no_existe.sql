0b77547684d22a6f696830e1fe8013d7cad2e652
Sergi Giribet <sergigiribet2005@ginebro.cat>
2023-11-03 17:22:26 +0100

    sql sequence

--- eduQuack/Server/sql/eduQuack_no_existe.sql
+++ eduQuack/Server/sql/eduQuack_no_existe.sql
@@ -1,40 +1,52 @@
+DROP DATABASE IF EXISTS eduQuack;
+
 CREATE DATABASE IF NOT EXISTS eduQuack;
 
 USE eduQuack;
 
-CREATE TABLE IF NOT EXISTS usuaris (
-  id_usuari INTEGER NOT NULL AUTO_INCREMENT, 
-  nom_usuari VARCHAR(255) NOT NULL, 
-  contrasenya VARCHAR(255) NOT NULL, 
-  nom VARCHAR(80) NOT NULL, 
-  cognoms VARCHAR(80) NOT NULL, 
-  PRIMARY KEY (id_usuari)
+CREATE TABLE usuarios (
+  id INTEGER NOT NULL AUTO_INCREMENT, 
+  email VARCHAR(255) NOT NULL,
+  full_name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL, 
+  username VARCHAR(80) NOT NULL, 
+  curso VARCHAR(80) NOT NULL,
+  edad INT NOT NULL, 
+  PRIMARY KEY (id)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS cursos (
-  id_curs INTEGER NOT NULL AUTO_INCREMENT, 
-  nom_curs VARCHAR(80) NOT NULL,
-  PRIMARY KEY (id_curs)
+CREATE TABLE cursos(
+  id_curso INTEGER NOT NULL AUTO_INCREMENT, 
+  nombre_curso VARCHAR(80) NOT NULL,
+  PRIMARY KEY (id_curso)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS blog (
-  id_blog INTEGER NOT NULL AUTO_INCREMENT, 
-  titol_blog VARCHAR(255) NOT NULL,
-  content_blog TEXT,
-  path_blog VARCHAR(255),
-  date_blog TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  author_blog VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id_blog)
+CREATE TABLE blog (
+  id INTEGER NOT NULL AUTO_INCREMENT, 
+  title VARCHAR(255) NOT NULL,
+  content TEXT,
+  image_path VARCHAR(255),
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+  author VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS noticia (
+-- CREATE TABLE codigos (
+--   id INTEGER NOT NULL AUTO_INCREMENT, 
+--   email VARCHAR(255) NOT NULL,
+--   codigo VARCHAR(6),
+--   creado_en TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+--   PRIMARY KEY (id)
+-- ) ENGINE=innodb DEFAULT CHARSET=utf8;
+3
+CREATE TABLE noticia (
   id_noticia INTEGER NOT NULL AUTO_INCREMENT, 
-  titol_noticia VARCHAR(255) NOT NULL,
-  content_noticia TEXT,
-  path_noticia VARCHAR(255),
-  date_noticia TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  author_noticia VARCHAR(255) NOT NULL,
+  id_curso INTEGER NOT NULL,
+  titulo VARCHAR(255),
+  contenido TEXT,
+  fecha DATE NOT NULL,
   PRIMARY KEY (id_noticia)
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 
-INSERT INTO usuaris (nom_usuari, contrasenya, nom, cognoms) VALUES ('root', '1234', 'root', 'root');
+-- Inserta datos
+INSERT INTO usuarios (email, full_name, password, username, curso, edad) VALUES ('root@ginebro.cat', 'root', '1234', 'root', 'root', 0);
