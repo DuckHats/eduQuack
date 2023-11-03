817e65f00b11195dc15e0bee9c79427f9c8d1df1
Sergi Giribet <sergigiribet2005@ginebro.cat>
2023-11-03 17:55:23 +0100

    database inserts

---
+++ eduQuack/Server/sql/Consulta.sql
@@ -0,0 +1,9 @@
+USE eduQuack;
+
+INSERT INTO cursos (nombre_curso) VALUES ('1ESO');
+INSERT INTO cursos (nombre_curso) VALUES ('2ESO');
+INSERT INTO cursos (nombre_curso) VALUES ('3ESO');
+INSERT INTO cursos (nombre_curso) VALUES ('4ESO');
+INSERT INTO cursos (nombre_curso) VALUES ('CicloMedio');
+INSERT INTO cursos (nombre_curso) VALUES ('CicloSuperior');
+INSERT INTO cursos (nombre_curso) VALUES ('Bachillerato');
